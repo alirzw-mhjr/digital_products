c5407600ae61f0372cf1a92b8c82e640b13cd225
alirzw-mhjr <alireza1379mohajer@gmail.com>
2026-02-12 20:52:02 +0300

    models added

--- products/models.py
+++ products/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Category(models.Model):
+    parent = models.ForeignKey('self', verbose_name=_('parent'), blank=True, null=True, on_delete=models.CASCADE)
+    title = models.CharField(_('title'),max_length=50)
+    description = models.TextField(_('description'), blank=True)
+    avatar = models.ImageField(_('avatar'), blank=True, upload_to='categories/')
+    is_enable = models.BooleanField(_('is enable'), default=True)
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time'), auto_now=True)
+
+    class Meta:
+        db_table = 'categories' #Имя таблицы в базе данных - The name of the table in the database
+        verbose_name = _('category') #Единственное название в админ-панеле - The singular name in admin panel
+        verbose_name_plural = _('Categories') #Множественное название в админ-панеле - The plural name in admin panel
+
+class Product(models.Model):
+    title = models.CharField(_('title'), max_length=50)
+    description = models.TextField(_('description'), blank=True)
+    avatar = models.ImageField(_('avatar'), blank=True, upload_to='products/')
+    is_enable = models.BooleanField(_('is enable'), default=True)
+    categories = models.ManyToManyField('Category', verbose_name=_('categories'), blank=True)
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time'), auto_now=True)
+
+    class Meta:
+        db_table = 'products' #название таблицы в базе данных - The name of the table in the database
+        verbose_name = _('product') #Единственное название в админ-панеле - The singular name in admin panel
+        verbose_name_plural = _('products') #Множественное название в админ-панеле - The plural name in admin panel
+
+
+
+class File(models.Model):
+    product = models.ForeignKey('Product', verbose_name=_('product'), on_delete=models.CASCADE)
+    title = models.CharField(_('title'), max_length=50)
+    file = models.FileField(_('file'), upload_to='files/%Y/%m/%d/')
+    is_enable = models.BooleanField(_('is enable'), default=True)
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time'), auto_now=True)
+
+    class Meta:
+        db_table = 'files' #название таблицы в базе данных - The name of the table in the database
+        verbose_name = _('file') #Единственное название в админ-панеле - The singular name in admin panel
+        verbose_name_plural = _('files') #Множественное название в админ-панеле - The plural name in admin panel
